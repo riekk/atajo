7c033fafacef0d7577a8754b6c2b92e4748def3a
riekk <115899694+riekk@users.noreply.github.com>
2023-02-08 17:41:15 +0100

    Solucionado suma de caminos
    Añadido un método para sumar correctamente los arrays de diferente tamaño.

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -1,62 +1,60 @@
 package gps;
+
 /**
  * Bicicleta class
- * 
- * @Java IES Severo Ochoa
- * @2.0
+ * @author Christian Torrecillas
+ * @Java IES Severo Ochoa @2.0
  */
 
 public class Bicicleta {
-    public Bicicleta(){
-        int ruedas = 4;
-        boolean luces = false;
-        String marca = "Orbea";
-    }
-    
-    public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
-    {
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        
-        for(int i=0; i<camino1.length; i++)
-        {
-            sum1 += camino1[i];
-            sum2 += camino2[i];
-            sum3 += camino3[i];
-        }
-        
-        if(sum1 < sum2 && sum1 < sum3)
-        {
-            return "camino 1"; 
-        }else if(sum2 < sum3 && sum2 < sum1)
-        {
-            return "camino 2";
-        }else if(sum3 < sum1 && sum3 < sum2)
-        {
-            return "camino 3";
-        }else{
-            if(sum1 == sum2 && sum2 == sum3)
-            {
-                return "camino 1, 2 y 3";
-            }else if(sum1 == sum3)
-            {
-                return "caminos 1 y 3";
-            }else if(sum2 == sum3){
-                return "caminos 2 y 3";
-            }else{
-                return "caminos 1 y 2";
-            }
-        }
-        
-    }
+	public Bicicleta() {
+		int ruedas = 4;
+		boolean luces = false;
+		String marca = "Orbea";
+	}
 
-    public static void main(String[] args) {
-        Bicicleta bici = new Bicicleta();
-        int[] camino1 = {12,34,21,46,25};
-        int[] camino2 = {24,1,5,64,10,15,21};
-        int[] camino3 = {1,5,6,7,8,10,20,46,104};
-        System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
-    }
+	public static String rutaCorta(int[] camino1, int[] camino2, int[] camino3) {
+		int sum1 = 0;
+		int sum2 = 0;
+		int sum3 = 0;
+
+		sum1 = Bicicleta.sumaRutas(camino1);
+		sum2 = Bicicleta.sumaRutas(camino2);
+		sum3 = Bicicleta.sumaRutas(camino3);
+
+		if (sum1 < sum2 && sum1 < sum3) {
+			return "camino 1";
+		} else if (sum2 < sum3 && sum2 < sum1) {
+			return "camino 2";
+		} else if (sum3 < sum1 && sum3 < sum2) {
+			return "camino 3";
+		} else {
+			if (sum1 == sum2 && sum2 == sum3) {
+				return "camino 1, 2 y 3";
+			} else if (sum1 == sum3) {
+				return "caminos 1 y 3";
+			} else if (sum2 == sum3) {
+				return "caminos 2 y 3";
+			} else {
+				return "caminos 1 y 2";
+			}
+		}
+
+	}
+
+	public static int sumaRutas(int[] camino) {
+		int suma = 0;
+		for (int i = 0; i < camino.length; i++) {
+			suma += camino[i];
+		}
+		return suma;
+	}
+
+	public static void main(String[] args) {
+		Bicicleta bici = new Bicicleta();
+		int[] camino1 = { 12, 34, 21, 46, 25 };
+		int[] camino2 = { 24, 1, 5, 64, 10, 15, 21 };
+		int[] camino3 = { 1, 5, 6, 7, 8, 10, 20, 46, 104 };
+		System.out.println("El camino más corto es:" + Bicicleta.rutaCorta(camino1, camino2, camino3) + ".");
+	}
 }
-
